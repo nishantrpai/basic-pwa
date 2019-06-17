955f042e74ae8d44389e9699ecacda112ddcbdaf
nishu <eiglesias914@gmail.com>
2019-06-17 06:56:17 +0530

    a basic pwa app is ready

---
+++ scripts/install.js
@@ -0,0 +1,52 @@
+(function () {
+    'use strict';
+
+
+    const Installer = function (root) {
+        let promptEvent;
+
+        const install = function (e) {
+            if (promptEvent) {
+                promptEvent.prompt();
+                promptEvent.userChoice
+                    .then(function (choiceResult) {
+                        // The user actioned the prompt (good or bad).
+                        // good is handled in 
+                        promptEvent = null;
+                        root.classList.remove('available');
+                    })
+                    .catch(function (installError) {
+                        // Boo. update the UI.
+                        promptEvent = null;
+                        root.classList.remove('available');
+                    });
+            }
+        };
+
+        const installed = function (e) {
+            promptEvent = null;
+            // This fires after onbeforinstallprompt OR after manual add to homescreen.
+            root.classList.remove('available');
+        };
+
+        const beforeinstallprompt = function (e) {
+            promptEvent = e;
+            promptEvent.preventDefault();
+            root.classList.add('available');
+            return false;
+        };
+
+        window.addEventListener('beforeinstallprompt', beforeinstallprompt);
+        window.addEventListener('appinstalled', installed);
+
+        root.addEventListener('click', install.bind(this));
+        root.addEventListener('touchend', install.bind(this));
+    };
+
+
+    window.addEventListener('load', function () {
+        const installEl = document.getElementById('installer');
+        const installer = new Installer(installEl);
+        let isLooping = false;
+    });
+})();
---
+++ sw.js
@@ -0,0 +1,35 @@
+const version = "0.6.19";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/manifest.json`,
+        `/index.css`,
+        `/app.js`,
+        `/sw.js`,
+        `/scripts/react.development.js`,
+        `/scripts/react-dom.development.js`,
+        `/scripts/pwacompat.min.js`,
+        `/scripts/install.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
